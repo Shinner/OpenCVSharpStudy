1bc24be5db78a4453c3eda803d40d518632600d5
Shinner <1021339627@qq.com>
2020-11-20 17:25:43 +0800

    add open image sample

---
+++ OpenCVS/OpenCVS/Case/01_OpenImage/Sample.cs
@@ -0,0 +1,17 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCVS.Case
+{
+    public partial class Sample
+    {
+        public void OpenImage()
+        {
+            Mat src = new Mat(@".\resources\sample.jpg");
+            Cv2.ImShow("src", src);
+            Cv2.WaitKey();
+        }
+    }
+}
---
+++ OpenCVS/OpenCVS/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenCVS.Case;
+using OpenCvSharp;
+using System;
+
+namespace OpenCVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Sample sample = new Sample();
+
+            sample.OpenImage();
+        }
+    }
+}
